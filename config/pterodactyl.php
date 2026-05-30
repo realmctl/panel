83c7a61a0acc29abb01cd0b6e287b112961de7d0
Stijn Jakobs <stijn@numblio.com>
2026-05-30 18:57:20 +0200

    chore(config): update CDN URL to ordnary.com
    - Update CDN endpoint URL from pterodactyl.io to ordnary.com/realmctl
    - Change protocol from HTTPS to HTTP for CDN releases endpoint
    - Redirect latest.json requests to new CDN infrastructure

--- config/pterodactyl.php
+++ config/pterodactyl.php
@@ -92,7 +92,7 @@ return [
 
     'cdn' => [
         'cache_time' => 60,
-        'url' => 'https://cdn.pterodactyl.io/releases/latest.json',
+        'url' => 'http://cdn.ordnary.com/realmctl/latest.json',
     ],
 
     /*
