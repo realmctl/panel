07622a7c5d6896ced48238bce4dbbe90f8924811
Stijn Jakobs <stijn@numblio.com>
2026-05-30 21:26:18 +0200

    fix: make pollingInterval non-static for Filament v5

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -11,7 +11,7 @@ use Filament\Widgets\StatsOverviewWidget\Stat;
 
 class StatsOverview extends StatsOverviewWidget
 {
-    protected static ?string $pollingInterval = '30s';
+    protected ?string $pollingInterval = '30s';
 
     protected function getStats(): array
     {
