ee4f19c724f819452129f0bfe9c5d3d060774c6c
Ezra Klijnsma <hakim.amellah@outlook.com>
2026-06-02 11:34:45 +0200

    feat(ux): Support for uploading folders directly inside of the file page. - Added support for uploading folders and their files through drag-and-drop functionality and selection - Expanded the logic to first create the folder and read the files of the uploaded folder directly

--- database/Seeders/DatabaseSeeder.php
+++ database/Seeders/DatabaseSeeder.php
@@ -13,5 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(NestSeeder::class);
         $this->call(EggSeeder::class);
+        $this->call(LocationSeeder::class);
+        $this->call(NodeSeeder::class);
+        $this->call(ServerSeeder::class);
     }
 }
