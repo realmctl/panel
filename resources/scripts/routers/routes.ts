939ed10ab5f5feed1116fd8b2c7eb9a5bccf20ae
stijnwtf <stijn@numblio.com>
2026-07-20 22:27:56 +0200

    refactor: Replace Permission Templates route with Environment route for improved navigation

--- resources/scripts/routers/routes.ts
+++ resources/scripts/routers/routes.ts
@@ -13,7 +13,7 @@ import SettingsContainer from '@/components/server/settings/SettingsContainer';
 import AccountOverviewContainer from '@/components/dashboard/AccountOverviewContainer';
 import AccountApiContainer from '@/components/dashboard/AccountApiContainer';
 import AccountSSHContainer from '@/components/dashboard/ssh/AccountSSHContainer';
-import AccountPermissionTemplatesContainer from '@/components/dashboard/permissionTemplates/AccountPermissionTemplatesContainer';
+import AccountEnvironmentContainer from '@/components/dashboard/environment/AccountEnvironmentContainer';
 import ActivityLogContainer from '@/components/dashboard/activity/ActivityLogContainer';
 import ServerActivityLogContainer from '@/components/server/ServerActivityLogContainer';
 import VersionChangerContainer from '@/components/server/versions/VersionChangerContainer';
@@ -65,9 +65,9 @@ export default {
             component: AccountSSHContainer,
         },
         {
-            path: '/permission-templates',
-            name: 'Permission Templates',
-            component: AccountPermissionTemplatesContainer,
+            path: '/environment',
+            name: 'Environment',
+            component: AccountEnvironmentContainer,
         },
         {
             path: '/activity',
